928fe0aac32d71e6f9876929a019499832d1d923
Priyanshu-Upadhyay-27 <160316707+Priyanshu-Upadhyay-27@users.noreply.github.com>
2026-04-11 23:28:30 +0530

    defined the code for loading model for once, at the starting only

--- api.py
+++ api.py
@@ -27,3 +27,25 @@ class CustomerData(BaseModel):
     MonthlyCharges: float
     TotalCharges: str
 
+RUN_ID = "64d602b049d4495a9eee58b189922a90"
+MASTER_PATH = Path("C:/Users/priya/Desktop/PyCh_Pro/Churn_Analysis_and_Modelling")
+DB_URI = f"sqlite:///{MASTER_PATH.as_posix()}/mlflow.db"
+
+app = FastAPI(title="Telecom Churn Production API")
+@app.on_event("startup")
+def load_model():
+    global model
+    try:
+        print(f"Connecting to database: {DB_URI}")
+        mlflow.set_tracking_uri(DB_URI)
+
+        model_uri = f"runs:/{RUN_ID}/model"
+        print(f"Loading model from: {model_uri}")
+
+        model = mlflow.sklearn.load_model(model_uri)
+        print("SUCCESS: Model loaded and ready for inference.")
+    except Exception as e:
+        print(f"CRITICAL ERROR: Could not load model. Details: {e}")
+        # This will keep the variable 'model' as None, causing /predict to fail safely
+
+
